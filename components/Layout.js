f664f823e717147e4c4de9faeadf2c7261fcbc94
lykemike <61860743+lykemike@users.noreply.github.com>
2021-03-16 10:54:03 +0700

    pindahin pages

--- components/Layout.js
+++ components/Layout.js
@@ -1,29 +1,28 @@
 import React from 'react'
 import Head from 'next/head'
 import Navbar from './Navbar'
+import Sidebar from './sidebar'
 import Login from './login'
 import Register from './register'
-import Sidebar from './sidebar'
-import CreateUser from './createform/createuser'
+import CreateRole from '../pages/createform/createrole'
+import CreateUser from '../pages/createform/createuser'
 
-export default function Layout({}) {
+
+export default function Layout({ }) {
     return (
-        
+
         <div className="bg-white-300">
-              <Navbar>
-                 </Navbar>
-            <Head> 
-           
-            <Sidebar>
-           
-              </Sidebar> 
-              </Head>
-           <div className="container mx-64">
+            <Navbar>
+            </Navbar>
+            <Head>
 
-            <CreateUser> 
+                <Sidebar>
 
-             </CreateUser>
-             </div>
+                </Sidebar>
+            </Head>
+            <div className="container mx-64">
+
+            </div>
         </div>
     )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,30 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  const express = require('express');
+  const mysql = require('mysql');
+
+  const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '12345',
+    database: 'nodemysql'
+  });
+
+
+  const app = express();
+
+  db.connect((err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('MySql Connected.....')
+  });
+
+  app.listen('3000', () => {
+    console.log('Server started on port 3000');
+  });
+
   return <Component {...pageProps} />
 }
 
